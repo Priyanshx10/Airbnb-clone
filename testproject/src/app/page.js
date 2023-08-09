6ba7a5ad9c0a768b10aca6893ef313abae81a89c
Priyansh <Priyanshyadav1012@gmail.com>
2023-08-09 20:38:22 +0530

    Update the logo and Import the Search box

---
+++ testproject/src/app/page.js
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+import Header from '../../component/Header'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+  <div className="">  
+  <main>
+    
+      <Head>
+        <title>AIRBNB</title>
+        <link rel ="icon" href="/favicon.ico"/>
+      </Head>
+
+         {/* Header */}
+      <Header/>
+      {/* Banner */}
+    
+  </main>
+  </div>
+  )
+}
