3130be0af2d29ef1f4cdc1ca4b387458368435dd
Priyansh <Priyanshyadav1012@gmail.com>
2023-08-10 02:55:36 +0530

    install Eslint and configured.

--- testproject/component/Header.js
+++ testproject/component/Header.js
@@ -1,24 +1,25 @@
 import Image from "next/image";
+import React from "react";
 
 function Header() {
     return(
+
         <header>
            {/* left */}
-           <div className='relative flex items-center h-10'>
+           <div className='relative flex items-center max-w-fit p-10'>
             <Image
             src ="https://links.papareact.com/qd3"
-            layout="fill"
-            objectFit="contain"
+            height={100}
+            width={100}
+            objectFit='contain'
             objectPosition="left"
-
-            
             />
            </div>
 
 
+
            {/* middle */}
            <div>
-
            </div>
 
 
