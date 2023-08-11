6af89b3b462e0c920a4558faa7dbc963b65fe6ba
Priyansh <Priyanshyadav1012@gmail.com>
2023-08-11 17:24:41 +0530

    Header Completed
    completed the header

--- testproject/src/app/page.tsx
+++ testproject/src/app/page.tsx
@@ -1,14 +1,16 @@
+/* eslint-disable prettier/prettier */
 import Head from "next/head";
+import React from "react";
 import Header from "./components/Header";
 
 export default function Home() {
   return (
     <div className="h-screen">
       <main className="">
-        <Head>
+        <head>
           <title>AIRBNB</title>
           <link rel="icon" href="/favicon.ico" />
-        </Head>
+        </head>
         {/* Header */}
         <Header />
         {/* Banner */}
