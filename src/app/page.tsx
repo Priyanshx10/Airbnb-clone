25d63b06a4ee0f3cf77265efc25e0d2a4da02b40
jefe-spain <jefe.1988@gmail.com>
2023-08-11 16:58:45 +0200

    chore: restructure data and quick fixes, change metadata and fix hydration

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+
+export default function Home() {
+  return (
+    <div className="h-screen">
+      <main className="">
+        <Header />
+        {/* Banner */}
+      </main>
+    </div>
+  );
+}
