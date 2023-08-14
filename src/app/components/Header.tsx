2fd4f66e4751c49c981b82c1affb6286eedea74f
Jesus Fernandez <jefernandez@theworkshop.com>
2023-08-14 22:15:54 +0200

    tweak: user menu flex row

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -34,7 +34,7 @@ function Header() {
       <div className="flex items-center space-x-4 justify-end text-gray-500">
         <p className="hidden md:inline cursor-pointer">Become a host</p>
         <GlobeAltIcon className="h-6 cursor-pointer" />
-        <div className="Flex items-center space-x-2 border-2 p-2 rounded-full">
+        <div className="items-center border-2 p-2 rounded-full flex">
           <MenuAlt1Icon className="h-6" />
           <UserCircleIcon className="h-6" />
         </div>
