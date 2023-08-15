0b5fece16f58edeb72fb77eccb1bca5a5390bd1f
Priyansh <Priyanshyadav1012@gmail.com>
2023-08-15 11:39:45 +0530

    latest update by jesus.

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable prettier/prettier */
+import './globals.css';
+import { Inter } from 'next/font/google';   
+import { Metadata } from 'next'
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Airbnb Clone",
+  description: "This is an Airbnb clone built with Next.js and Tailwind CSS.",
+}
+
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
