3a03e7c18c3639d9ffb0cbc0e401f24e22011d36
Jesus Fernandez <Jefe.1988@gmail.com>
2023-08-21 12:24:54 +0200

    change getstaticprops (#6)
    Co-authored-by: Jesus Fernandez <jefernandez@theworkshop.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,40 @@
- /* eslint-disable prettier/prettier */
- 
- import Head from "next/head";
- import React from "react";
- import Header from "./components/Header";
- import Banner from "./components/Banner";
- 
- export default function Home({exploreData}) {
-   return (
-       <div className="">
-       
-         <head>
-           <title>AIRBNB</title>
-           <link rel="icon" href="/favicon.ico" />
-         </head>
-         {/* Header */}
-         <Header />
-         {/* Banner */}
-         <Banner/>
- 
-       <main className="max-w-7xl mx-auto px-8 sm:px-16 ">  
-         <section className="p-06">
-           <h2 className="text-3xl font-semibold">Explore Nearby</h2>
- 
-           {/* Pull some data from a server - API are already created. */}
-           {exploreData?.map((item) => (
-           <h1>{item.location}</h1>
-           ))}
-         </section>
-       </main>
-     </div>
-   );
- }
- 
- export async function getstaticProps() {
-   const exploreData = await fetch("https://links.papareact.com/pyp").
-   then(
-     (res) =>res.json()
-   );
- 
-   return {
-     props: {
-       exploreData
-     }
-   }
- }
- 
+import React from 'react';
+import Header from './components/Header';
+import Banner from './components/Banner';
+
+type ExploreDataType = {
+  img: string;
+  location: string;
+  distance: string;
+};
+
+const getData = async () => {
+  const res = await fetch('https://links.papareact.com/pyp');
+  const data: ExploreDataType[] = await res.json();
+  return data;
+};
+
+export default async function Home() {
+  const exploreData: ExploreDataType[] = await getData();
+  return (
+    <div className="">
+      {/* Header */}
+      <Header />
+      {/* Banner */}
+      <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16 ">
+        <section className="p-06">
+          <h2 className="text-3xl font-semibold">Explore Nearby</h2>
+
+          {/* Pull some data from a server - API are already created. */}
+          {exploreData?.map((item) => (
+            <div key={item.distance} className="w-10 h-10">
+              <h1>{item.location}</h1>
+            </div>
+          ))}
+        </section>
+      </main>
+    </div>
+  );
+}
