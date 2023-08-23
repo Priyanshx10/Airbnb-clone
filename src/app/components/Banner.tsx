c3c574b74616875a48101cb51133750972ab9d2f
Jesus Fernandez <jefernandez@theworkshop.com>
2023-08-23 11:34:23 +0200

    solve head and async data

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -1,27 +1,29 @@
-/* eslint-disable prettier/prettier */
-
-/* eslint-disable prettier/prettier */
-import Image from "next/image";
+import Image from 'next/image';
 import React from 'react';
 
-
 function Banner() {
-    return(
-        <div className=" relative h-[300px] sm:h-[400px] lg:h=[500px] xl:h-[600px] 2xl:h-[700px]">
-            <Image
-             src="https://links.papareact.com/0fm"
-             alt ="Banner"
-             layout="fill"
-             objectFit="cover"
-            />
-            <div className="absolute top-1/2 w-full text-center">
-                <p className="text-sm sm:text-md text-black ">Not Sure where to go ? Perfect.</p>
-            <button type="button" className=" text-purple-800 bg-white py-06 px-10 shadow-md
+  return (
+    <div className=" relative h-[300px] sm:h-[400px] lg:h=[500px] xl:h-[600px] 2xl:h-[700px]">
+      <Image
+        src="https://links.papareact.com/0fm"
+        alt="Banner"
+        layout="fill"
+        objectFit="cover"
+      />
+      <div className="absolute top-1/2 w-full text-center">
+        <p className="text-sm sm:text-md text-black ">
+          Not Sure where to go ? Perfect.
+        </p>
+        <button
+          type="button"
+          className=" text-purple-800 bg-white py-06 px-10 shadow-md
              rounded-full font-bold my-3 hover:shadow-xl 
-             active:scale-90 transition duration-150">I am flexible.</button>
-            </div>
-
-        </div>
-    );
-    
-} export default Banner;
+             active:scale-90 transition duration-150"
+        >
+          I am flexible.
+        </button>
+      </div>
+    </div>
+  );
+}
+export default Banner;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,35 @@
-/* eslint-disable prettier/prettier */
- import Head from "next/head";
- import React from "react";
- import Header from "./components/Header";
- import Banner from "./components/Banner";
- 
- export default function Home({exploreData}) {
-   return (
-       <div className="">
-       
-         <head>
-           <title>AIRBNB</title>
-           <link rel="icon" href="/favicon.ico" />
-         </head>
-         {/* Header */}
-         <Header />
-         {/* Banner */}
-         <Banner/>
- 
-       <main className="max-w-7xl mx-auto px-8 sm:px-16 ">  
-         <section className="p-06">
-           <h2 className="text-3xl font-semibold">Explore Nearby</h2>
- 
-           {/* Pull some data from a server - API are already created. */}
-           {exploreData?.map((item) => (
-           <h1>{item.location}</h1>
-           ))}
-         </section>
-       </main>
-     </div>
-   );
- }
- 
- export async function getstaticProps() {
-   const exploreData = await fetch("https://links.papareact.com/pyp").
-   then(
-     (res) =>res.json()
-   );
- 
-   return {
-     props: {
-       exploreData
-     }
-   }
- }
- 
+import React from 'react';
+import Header from './components/Header';
+import Banner from './components/Banner';
+
+const getData = async () => {
+  const exploreData = await fetch('https://links.papareact.com/pyp').then(
+    (res) => res.json(),
+  );
+  return exploreData;
+};
+
+const Page = async () => {
+  const data = await getData();
+  return (
+    <div className="">
+      {/* Header */}
+      <Header />
+      {/* Banner */}
+      <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16 ">
+        <section className="p-06">
+          <h2 className="text-3xl font-semibold">Explore Nearby</h2>
+
+          {/* Pull some data from a server - API are already created. */}
+          {data?.map((item: { location: string }) => (
+            <h1 key={item.location.toString()}>{item.location}</h1>
+          ))}
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default Page;
