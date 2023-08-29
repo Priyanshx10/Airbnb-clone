c183eeb6d266ecf449f74bc5ef83fabb3fc6217a
Jesus Fernandez <jefernandez@theworkshop.com>
2023-08-29 08:57:37 +0200

    resolve linting

--- src/app/components/SmallCard.tsx
+++ src/app/components/SmallCard.tsx
@@ -1,32 +1,29 @@
-/* eslint-disable prettier/prettier */
-import Image from "next/image";
+import Image from 'next/image';
 
-interface SmallCardProps{
-    img: string;
-    location: string;
-    distance: string;
+type SmallCardProps = {
+  img: string;
+  location: string;
+  distance: string;
 }
-function SmallCard({img ,location ,distance}: SmallCardProps) {
-    return(
-        <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-200 hover:scale-105 transform duration-200 ease-out">
-            {/* left */}
-             <div  className="relative h-16 w-16">
-                <Image
-                src={img}
-                layout="fill"
-                alt="SmallCardImages"
-                className="rounded-lg"
-                />
-             </div>
-            
-            {/*  Right */}
-             <div>
-             <h2>{location}</h2>
-             <h3>{distance}</h3>
-             </div>
-        </div>
-       
-    );
-    
+function SmallCard({ img, location, distance }: SmallCardProps) {
+  return (
+    <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-200 hover:scale-105 transform duration-200 ease-out">
+      {/* left */}
+      <div className="relative h-16 w-16">
+        <Image
+          src={img}
+          layout="fill"
+          alt="SmallCardImages"
+          className="rounded-lg"
+        />
+      </div>
+
+      {/*  Right */}
+      <div>
+        <h2>{location}</h2>
+        <h3>{distance}</h3>
+      </div>
+    </div>
+  );
 }
 export default SmallCard;
