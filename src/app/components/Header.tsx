c40eb7f2abdee19592a2085d897ae55c588d047c
Jesus Fernandez <jefernandez@theworkshop.com>
2023-08-29 08:56:12 +0200

    resolve linting

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable prettier/prettier */
-
 import Image from 'next/image';
 import {
   SearchIcon,
@@ -10,8 +8,7 @@ import {
 
 function Header() {
   return (
-    
-    <header className=" sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">    
+    <header className=" sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* left */}
       <div className="relative flex items-center h-10 cursor-pointer my-auto ">
         <Image
